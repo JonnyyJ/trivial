ff6b398c8f889578cf77b71c7385d5dbba9fd056
Jonnyy <jonnyy@jonnyys-mbp.resource.campus.njit.edu>
2018-12-15 18:20:14 -0500

    Bubble Sort

--- Commonly alg/Bubble Sort.js
+++ Commonly alg/Bubble Sort.js
@@ -41,3 +41,20 @@ function bubbleSort2(arr){
 // test
 const arr = [91, 60, 96, 7, 35, 65, 10, 65, 9, 30, 20, 31, 77, 81, 24];
 console.log(bubbleSort2(arr));
+
+//for bubble sort, can u pass the second parameter(function) to control the ascending and descending
+function bubbleSort(arr, compareFunc){
+    for(let i = arr.length - 1; i > 0; i--){
+        for (let j = 0; j < i; j++){
+            if(compareFunc(arr[j], arr[j + 1]) > 0){
+                swap(arr, i, j+1);
+            }
+        }
+    }
+    return arr;
+}
+// test
+const arr = [91, 60, 96, 7, 35, 65, 10, 65, 9, 30, 20, 31, 77, 81, 24];
+console.log(bubbleSort(arr, (a, b) => a - b));
+console.log(bubbleSort(arr, (a, b) => b - a));
+
