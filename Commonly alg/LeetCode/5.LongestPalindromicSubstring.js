5b0b6b9c7ad7226c36444501bc30d3841425b7a0
Jonnyy <jonnyy@Jonnyys-MacBook-Pro.local>
2019-01-07 14:23:27 +0800

    longestPalindrome

--- Commonly alg/LeetCode/5.LongestPalindromicSubstring.js
+++ Commonly alg/LeetCode/5.LongestPalindromicSubstring.js
@@ -19,3 +19,29 @@ var longestPalindrome = function(s) {
     }
     return res;
 };
+
+var longestPalindrome = function(s){
+    let res = ""
+    function checkPalindrome(l, r){
+      while (l >= 0 && r < s.length){
+              if (s[l] == s[r]){
+                  l -= 1;
+                  r += 1;
+              } else{
+                  break;
+              }
+          }
+          l += 1;
+          r -= 1;
+          if (r - l + 1 > res.length){
+              res = s.substring(1, r + 1);
+          }
+      }
+      for (let i = 0; i < s.length; i++){
+          if (i > 0 && s[i] == s[i - 1]){
+              checkPalindrome(i - 1, i);
+          }
+          checkPalindrome(i, i);
+      }
+      return res;
+    }
