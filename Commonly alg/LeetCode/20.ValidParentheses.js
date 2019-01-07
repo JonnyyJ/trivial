166b198de4036325111db26a8887037e5985b742
Jonnyy <jonnyy@Jonnyys-MacBook-Pro.local>
2019-01-07 17:30:36 +0800

    ValidParentheses

---
+++ Commonly alg/LeetCode/20.ValidParentheses.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+ */
+var isValid = function(s) {
+    let length = s.length;
+    while (length > 0 && s.length) {
+      s = s.replace(/\[\]|\{\}|\(\)/g, '');
+      length -= 2;
+    }
+    return s === ''
+  };
