e2794de640e7d1c8f7ce38fe045d0fb2188bd221
Jonnyy <jonnyy@Jonnyys-MacBook-Pro.local>
2019-01-10 16:41:10 +0800

    25

---
+++ test/25.customInputComponents.js
@@ -0,0 +1,41 @@
+//<coupon v-model="coupon"></coupon>
+
+Vue.component('coupon', {
+
+    props: ['code'],
+
+    template: `
+    <input type="text" :value="code" @input="updateCode($event.target.value)" ref="input">
+    `,
+
+    data(){
+        return{
+           invalids: ['ALL', 'something']
+        }
+    },
+    methods: {
+        updateCode(code){
+
+            if(this.invalids.includes(code)){
+                alert('go fuck urself')
+
+                this.$refs.input.value = code = ``;
+            }
+          
+             
+            this.$emit('input', code);
+        }
+    }
+})
+
+
+
+
+new Vue({
+    
+    el: '#app',
+
+    data:{
+        coupon: 'freebie'
+    }
+})
