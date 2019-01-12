deeba71e1eed7f26187d02bbc8101ef846c8893b
Jonnyy <jonnyy@Jonnyys-MacBook-Pro.local>
2019-01-12 18:12:08 +0800

    test

--- Commonly alg/LeetCode/6.ZigZagConversion.js
+++ Commonly alg/LeetCode/6.ZigZagConversion.js
@@ -17,3 +17,17 @@ const convert = (s, n) => {
     }
     return res.map(row => row.join('')).join('')
 };
+
+const convert = (s, n) => {
+    const res = []
+    let r = 0, dir = n > 1 ? 1 : 0
+    for (const c of s ) {
+        res[r] = res[r] || []
+        res[r].push(c)
+        if(r === 0 && dir === -1 || r === n - 1 && dir === 1){
+               dir *= -1
+        }
+        r += dir
+    }
+    return res.map(row => row.join('')).join('')
+};
