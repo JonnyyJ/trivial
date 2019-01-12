782fde16a8624e3626cdc5d26b710175d24cffbd
Jonnyy <jonnyy@Jonnyys-MacBook-Pro.local>
2019-01-12 11:21:49 +0800

    spa

---
+++ test/27.spa/Home.vue
@@ -0,0 +1,16 @@
+<script>
+
+  import moment from 'moment';
+  import Status from '../models/Status';
+export default {
+    data(){
+        return {
+            statuses: []
+        }
+    },
+
+    created() {
+        Status.all(statuses => this.wstatuses = statuses);
+    },
+}
+</script>
