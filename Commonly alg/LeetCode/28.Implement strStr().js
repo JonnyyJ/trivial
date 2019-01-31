5e0bcc4b441c47746e94cb8ff723e751d8d98384
Jonnyy <caijishiwo@gmail.com>
2019-01-30 22:02:32 -0500

    Implement strStr()

---
+++ Commonly alg/LeetCode/28.Implement strStr().js
@@ -0,0 +1,20 @@
+/**
+ * Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, 
+or -1 if needle is not part of haystack.
+ */
+function strStr(haystack, needle) {
+    if (!needle) {
+      return 0;
+    }
+  
+    // Look for the needle at each position
+    for (let i = 0; i < haystack.length - needle.length + 1; i++) {
+      if (haystack.slice(i, i + needle.length) === needle) {
+        return i;
+      }
+    }
+  
+    return -1;
+  }
