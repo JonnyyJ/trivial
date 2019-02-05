8b36bca1848d98be5dffdcae515b2a7f5d95c785
Jonnyy <caijishiwo@gmail.com>
2019-02-05 07:38:37 -0500

    LongestValidParenthese

---
+++ Commonly alg/LeetCode/32.LongestValidParenthese.js
@@ -0,0 +1,26 @@
+/*Given a string containing just the characters '(' and ')',
+ *  find the length of the longest valid (well-formed) 
+ * parentheses substring.
+ */
+
+
+var longestValidParentheses = function(s) {
+    let ret = 0
+    const stack = [0]
+    s.split('').forEach(c => {
+        if (c === '('){
+            stack.push(0)
+        }else {
+           const len = stack.length
+           if (len >= 2){
+               stack[len - 2] += stack.pop() + 2
+               ret = Math.max(ret, stack[len - 2])
+           }  else if (len === 1){
+               stack[0] = 0
+           } else {
+               throw new Error('will not happen')
+           }
+        }
+    })
+    return ret
+}
