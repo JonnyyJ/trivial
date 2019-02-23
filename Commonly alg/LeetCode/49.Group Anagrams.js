8d23868793a5264f6b2e3ac81c1bf63683cdd958
Jonnyy <caijishiwo@gmail.com>
2019-02-22 23:52:33 -0500

    GroupAnagrams

--- Commonly alg/LeetCode/49.Group Anagrams.js
+++ Commonly alg/LeetCode/49.Group Anagrams.js
@@ -2,6 +2,18 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
+const groupAnagrams = strs => {
+    const map = {};
     
+    for (let str of strs) {
+        const key = [...str].sort().join('');
+
+        if (!map[key]) {
+            map[key] = [];
+        }
+
+        map[key].push(str);
+    }
+    
+    return Object.values(map);
 };
