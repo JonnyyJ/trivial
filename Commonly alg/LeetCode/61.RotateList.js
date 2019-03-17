99b61b59495eeeaf75880d4f5a75b7e0fbf5df8b
Jonnyy <caijishiwo@gmail.com>
2019-03-16 23:27:49 -0400

    RotateList

---
+++ Commonly alg/LeetCode/61.RotateList.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    let tail = head;
+    if (head === null) return head;
+    let len = 1;
+    while (tail.next) {
+      tail = tail.next;
+      len++;
+    }
+    tail.next = head;
+    let count = len - (k % len);
+    while (count > 0) {
+      head = head.next;
+      tail = tail.next;
+      count--;
+    }
+    tail.next = null;
+    return head;
+  };
