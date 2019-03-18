6fa08824108a076cdc73dae75afddc26ce77d6f7
Jonnyy <caijishiwo@gmail.com>
2019-03-18 15:42:26 -0400

    Amazon 3-oa2

--- Commonly alg/LeetCode/976.KClosestPointsToOrigin.js
+++ Commonly alg/LeetCode/976.KClosestPointsToOrigin.js
@@ -5,5 +5,5 @@
  * We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
  */
 var kClosest = function(points, K) {
-    return points.sort((a, b) => a[0]*a[0]+a[1]*a[1] - b[0]*b[0] - b[1]*b[1]).slice(0, k);
+    return points.sort((a, b) => a[0]*a[0]+a[1]*a[1] - b[0]*b[0] - b[1]*b[1]).slice(0, K);
 };
