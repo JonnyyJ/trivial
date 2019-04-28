8f3e04d0b82aa3d2a98956bb0d7f507f4ffce7b8
Jonnyy <caijishiwo@gmail.com>
2019-04-28 18:19:05 -0400

    102

--- Commonly alg/LeetCode/102.Binary Tree Level Order Traversal.js
+++ Commonly alg/LeetCode/102.Binary Tree Level Order Traversal.js
@@ -1,1 +1,34 @@
-function 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+var levelOrder = function(root) {
+    let result = [];
+    currentLevelNodes = [];
+    if(root) 
+        currentLevelNodes.push(root);
+    while(currentLevelNodes.length > 0) {
+        current = [];
+        let len = currentLevelNodes.length;
+        for (let i = 0; i< len; i++) {
+            let node = currentLevelNodes.shift();
+            current.push(node.val);
+            if(node.left) {
+                currentLevelNodes.push(node.left);
+            }
+            if(node.right) {
+                currentLevelNodes.push(node.right);
+            }
+        }
+        result.push(current);
+    }
+    return result;
+};
