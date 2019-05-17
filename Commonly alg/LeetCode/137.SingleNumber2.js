c077632e79c8266a41e5b8e59dcd61a180382374
Jonnyy <caijishiwo@gmail.com>
2019-05-16 23:40:03 -0400

    singleNumber2

---
+++ Commonly alg/LeetCode/137.SingleNumber2.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    var map = new Map()
+    for (let num of nums){
+        let cur = map.get(num) || 0
+        if(cur && cur === 2) {
+            map.delete(num)
+        }else{
+            map.set(num, ++cur)
+        }
+    }
+    return [...map][0][0]
+};
