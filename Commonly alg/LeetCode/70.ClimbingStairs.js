52276f9f02ee14498478a44461143f18db9bba32
Jonnyy <caijishiwo@gmail.com>
2019-05-27 23:50:52 -0400

    re 70

--- Commonly alg/LeetCode/70.ClimbingStairs.js
+++ Commonly alg/LeetCode/70.ClimbingStairs.js
@@ -2,19 +2,20 @@
  * @param {number} n
  * @return {number}
  */
+
 var climbStairs = function(n) {
-    if(n === 0 || n === 1) return 1;
-    if(n === 2) return 2;
-    let count = 2;
-    let prev = 2;
-    let prevPrev = 1;
-    let sum = 0;
-    while(count < n){
-        sum = prev + prevPrev;
-        prevPrev = prev;
-        prev = sum;
-        count++;
+  
+    if (n < 1) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+
+    // a saves the second-to-last sub-state data, b saves the first sub-state data, temp saves the current state data
+    let a = 1, b = 2;
+    let temp = a + b;
+    for (let i = 3; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp; 
     }
-    return sum;
-    
+    return temp; 
 };
