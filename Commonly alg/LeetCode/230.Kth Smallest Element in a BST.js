ce5123b5863233cefa7bd3fb1d4f8474fe9906ef
Jonnyy <caijishiwo@gmail.com>
2019-06-02 23:48:23 -0400

    230

---
+++ Commonly alg/LeetCode/230.Kth Smallest Element in a BST.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    let result = [];
+    helper(root)
+    return result[k - 1];
+    
+    function helper(node) {
+        if (!node) return;
+        if (result.length >= k) return;
+        
+        helper(node.left);
+        if (result.length >= k) return;
+        result.push(node.val);
+        helper(node.right);
+    }
+};
