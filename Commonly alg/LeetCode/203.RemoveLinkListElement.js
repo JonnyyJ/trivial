81bd5291ce919639289bf6d33e7ee735309ab94e
Jonnyy <caijishiwo@gmail.com>
2019-06-06 23:25:42 -0400

    203

---
+++ Commonly alg/LeetCode/203.RemoveLinkListElement.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    const root = {};
+    root.next = head;
+    let prev = root;
+    let cur = root.next;
+    while (cur) {
+      if (cur.val === val) {
+        prev.next = cur.next;
+        cur = cur.next;
+      } else {
+        prev = cur;
+        cur = cur.next;
+      }
+    }
+    
+    return root.next;
+  };
