dd1753dee7395dae0c0bc30ae3cf0f72f09cfccd
Jonnyy <caijishiwo@gmail.com>
2019-07-26 23:20:21 -0400

    605

---
+++ Commonly alg/LeetCode/605.CanPlaceFlowers.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    if(n === 0) return true;
+    for(var i = 0; i < flowerbed.length; i++){
+        if(flowerbed[i] === 0 && flowerbed[i + 1] !== 1 && flowerbed[i - 1] !== 1){
+            flowerbed[i] = 1;
+            n--;
+        }
+        if(n === 0){
+            return true;
+        }
+    }
+    return false;
+};
