44b7be8a5a63b4792a864237b5e1fd9bd3bafa6c
Jonnyy <caijishiwo@gmail.com>
2019-08-09 18:51:24 -0400

    437

---
+++ Commonly alg/LeetCode/437.PathSumIII.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number}
+ */
+var pathSum = function(root, sum) {
+    let pathSum = function(root, sum) {
+        if (!root) return 0;
+        return sumHelper(root, 0, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+    };
+    
+    let sumHelper = (root, pre, sum) => {
+      if (!root) return 0;
+      let total  = pre + root.val;
+      return (total === sum) + sumHelper(root.left, total, sum) + sumHelper(root.right, total, sum);
+    };
+};
