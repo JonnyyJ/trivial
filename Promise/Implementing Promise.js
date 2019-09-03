997e23593e44cab3cc2d87ef807b9ac3dc8c0bb5
Jonnyy <caijishiwo@gmail.com>
2019-09-03 18:44:35 -0400

    updated

--- Promise/Implementing Promise.js
+++ Promise/Implementing Promise.js
@@ -11,20 +11,33 @@ getUserID().then((id)=>{
    //method
 })
 
-//Simple Promise implementing
-function Promise(fn) {
-    var value = null,
-        callbacks = [];  //
-
-    this.then = function (onFulfilled) {
-        callbacks.push(onFulfilled);
-    };
-
-    function resolve(value) {
-        callbacks.forEach(function (callback) {
-            callback(value);
-        });
+//Full Promie implementation
+class MyPromise{
+    constructor (handle){
+        if(!isFunction(handle)){
+            throw new Error('MyPromise must accept a function as a parameter')
+        }
+        //add status
+        this._status = PENDING
+        //add status
+        this._value = undefined
+        //excute handle
+        try{
+            handle(this._resolve.bind(this), this._reject.bind(this))
+        } catch (err){
+            this._reject(err)
+        }
     }
-
-    fn(resolve);
+    //add function when resovle is excuted
+    _resolve(val){
+        if (this._status !== PENDING) return
+        this.status = FULFILLED
+        this._value = val
+    }
+    //add function when reject is excuted
+    _reject(err){
+        if(this._status !== PENDING) return
+        this._status = REJECTED
+        this._value = err
+    }
 }
