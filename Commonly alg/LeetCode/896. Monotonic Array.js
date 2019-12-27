824dcf8b4da333698e171ce4dc6908914fc8840a
Jonnyy <caijishiwo@gmail.com>
2019-12-26 23:55:05 -0500

    896

---
+++ Commonly alg/LeetCode/896. Monotonic Array.js
@@ -0,0 +1,8 @@
+var isMonotonic = function(A) {
+    let increase = true, decrease = true;
+    for(let i = 0; i < A.length - 1; i++) {
+        if(A[i] > A[i + 1]) increase = false;
+        if(A[i] < A[i + 1]) decrease = false;
+    }
+    return increase || decrease;
+};
