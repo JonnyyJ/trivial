dccd13c698754b1ad5f4be5055980db68d17071f
Jonnyy <caijishiwo@gmail.com>
2020-02-10 23:57:03 -0500

    update

--- Commonly alg/ClassicFrontEnd/countAndStay.js
+++ Commonly alg/ClassicFrontEnd/countAndStay.js
@@ -1,3 +1,17 @@
 var countAndStay = function(n){
-    
+    var resultStr = '1'
+    for(var i = 1; i < n; i++){
+        var repeatCount = 1;
+        var str = ''
+        for(var j = 0; j < resultStr.length; j++){
+            if(resultStr[j] === resultStr[j+1]){
+                repeatCount++
+            }else{
+                str += repeatCount + resultStr[j]
+                repeatCount = 1
+            }
+        }
+        resultStr = str
+    }
+    return resultStr
 }
