b350ebfb46fb8bea77639b0d139df1f79347c4b9
Jonnyy <caijishiwo@gmail.com>
2020-02-29 19:44:33 -0500

    442

--- Commonly alg/LeetCode/442. Find All Duplicates in an Array.js
+++ Commonly alg/LeetCode/442. Find All Duplicates in an Array.js
@@ -3,5 +3,15 @@
  * @return {number[]}
  */
 var findDuplicates = function(nums) {
-    
+	let result = [];
+	let len = nums.length;
+	let obj = {};
+	for (let i = 0; i < len; i++) {
+		let num = nums[i];
+		if (num in obj) {
+			result.push(num);
+		}
+		obj[num] = num;
+	}
+	return result;
 };
