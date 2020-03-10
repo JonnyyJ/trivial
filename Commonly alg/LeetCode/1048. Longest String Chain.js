148838bf911297dd5e95de1e87b738be75c81b86
Jonnyy <caijishiwo@gmail.com>
2020-03-10 03:15:59 -0400

    1048

--- Commonly alg/LeetCode/1048. Longest String Chain.js
+++ Commonly alg/LeetCode/1048. Longest String Chain.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var longestStrChain = function(words) {
-    
+    let dp = {};
+    words = words.sort((a,b)=>a.length - b.length);
+    for (let word of words) {
+        let longest = 0;
+        for (let i=0;i<word.length;i++) {
+            let pre = word.slice(0,i) + word.slice(i+1);
+            longest = Math.max(longest, (dp[pre]||0) + 1);
+        }
+        dp[word] = longest;
+    }
+    return Math.max(...Object.values(dp));
 };
